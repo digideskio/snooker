15e8a81bc6e31003572856e71963642719267b0d
Steven Occhipinti <dev@stevenocchipinti.com>
2014-05-18 19:43:43 +1000

    Scaffold an Angular app with `yo angular`
    Needed to do a couple of extra bits to make it work:
      https://stackoverflow.com/questions/22367059/no-provider-for-frameworkjasmine-resolving-frameworkjasmine

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('snookerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
