6cf4b92a5901e01b78c30a23819c92c5b9f12058
Steven Occhipinti <steve.occhipinti@rea-group.com>
2014-05-22 23:45:29 +1000

    Make the new-game screen use localStorage

--- app/scripts/controllers/new-game.js
+++ app/scripts/controllers/new-game.js
@@ -1,16 +1,48 @@
 'use strict';
 
 angular.module('snookerApp')
-  .controller('NewGameCtrl', function ($scope) {
-    $scope.players = [];
+  .controller('NewGameCtrl', function ($scope, $location) {
+
+    // TODO: Extract a Game object/service
 
     $scope.addPlayer = function() {
       $scope.players.push({name: $scope.playerName, target: $scope.targetScore});
+      $scope.saveGame();
       $scope.playerName = '';
-      $scope.targetScore = '';
+      $scope.targetScore = 31;
     };
 
     $scope.removePlayer = function(idx) {
       $scope.players.splice(idx, 1);
+      $scope.saveGame();
     };
+
+
+    $scope.resetScores = function() {
+      angular.forEach($scope.players, function(player) {
+        player.score = 0;
+      });
+    };
+
+    $scope.saveGame = function() {
+      $scope.resetScores();
+      localStorage.setItem('snookerGame', angular.toJson({
+        players: $scope.players,
+        timestamp: new Date().getTime()
+      }));
+    };
+
+    $scope.startGame = function() {
+      $location.path('/game/');
+    };
+
+
+    $scope.getPlayersFromGame = function() {
+      var game = JSON.parse(localStorage.getItem('snookerGame'));
+      return game && game.players;
+    };
+
+    $scope.players = $scope.getPlayersFromGame() || [];
+    $scope.playerName = '';
+    $scope.targetScore = 31;
   });
